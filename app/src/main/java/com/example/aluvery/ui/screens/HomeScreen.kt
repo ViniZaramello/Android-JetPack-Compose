f2ccffb703a2e3e7822fafb2aaa9476b7d3df255
oiachuva5 <oiachuva5@gmail.com>
2024-02-29 22:17:53 -0300

    Implementado o Lazy na ProductSection. Removido for que cria os cards de produtos. Adicionado método mais performatico que o for para criar cards no ProductSections

--- app/src/main/java/com/example/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/aluvery/ui/screens/HomeScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.aluvery.model.ProductModel
-import com.example.aluvery.sampledata.sampleProducts
 import com.example.aluvery.sampledata.sampleSections
 import com.example.aluvery.ui.components.ProductSection
 import com.example.aluvery.ui.theme.AluveryTheme
